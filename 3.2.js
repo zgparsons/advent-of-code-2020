f8f447a55c91a234e2a8c8b6e92c86518d8342c9
zgparsons <z.g.parsons@outlook.com>
2020-12-29 14:09:43 +0000

    Remove commented out code

--- 3.2.js
+++ 3.2.js
@@ -35,27 +35,3 @@ function traverse(lines, right, down) {
     }
     return numTrees;
 }
-
-// // Declare return variable, and x and y 'axes', plus get last in row index
-// let numTrees = 0;
-// let x = 0;
-// let y = 0;
-// let lastInRow = lines[0].length - 1;
-
-// // Begin at y=0 and loop all the way until y=final line and...
-// // ...move x along by 3 each time
-// while (y < lines.length - 1) {
-//     // Correct x if at the end of line
-//     if ((lastInRow - x) < 3) {
-//         x = x - lastInRow - 1;
-//     }
-//     x = x + 3;
-//     y = y + 1;
-//     // If current position is '#' add to count
-//     if (lines[y][x] === "#") {
-//         numTrees++;
-//     }
-// }
-
-// // Answer:
-// console.log(numTrees)
